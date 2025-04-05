44471cc5f729516906f8366d48041d741a866905
bryanmaina <sourcedeep1@gmail.com>
2025-04-05 23:27:08 +0200

    fix persist theme

---
+++ lua/chadrc.lua
@@ -0,0 +1,36 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+  theme = "eldritch",
+
+  hl_override = {
+  	Comment = { italic = true },
+  	["@comment"] = { italic = true },
+  },
+}
+--
+-- M.mason = {
+--   cmd = true,
+--   pkgs = {
+--     "prettierd",
+--     "prettier",
+--     "emmet-ls",
+--     "tailwindcss-language-server",
+--     "rust-analyzer",
+--     "jdtls",
+--     "dockerfile-language-server",
+--     "xmlformatter",
+--     "docker-compose-language-service",
+--     "stylua",
+--     "typescript-language-server",
+--     "lua-language-server",
+--     "google-java-format"
+--   },
+-- }
+
+return M
